550fea51460e3c4c677d74f8b2668b7b5671cdde
Krystian Brzoza <krystian.brzoza@outlook.com>
2020-03-06 12:57:05 +0100

    Adding database connection to config file

--- config/config.php
+++ config/config.php
@@ -1,7 +1,10 @@
 <!-- Config File for connecting to database and secure session -->
 <?php 
-    $database = '';
-    $login = '';
-    $password = '';
+    try{
+        $db = new PDO('mysql:host=localhost;dbname=website;charset=utf8mb4', 'root', 'password');
+     }
+     catch(PDOException $e){
+         die("Nie udalo sie polaczyc do bazy danych");
+     }
 
 ?>
