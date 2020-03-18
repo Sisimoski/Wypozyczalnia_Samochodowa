fc722f461c0276e40b6174350e7f284dd5e2c421
knammert <knammert@gmail.com>
2020-03-18 14:35:30 +0100

    Połowa Walidacji

--- js/register.js
+++ js/register.js
@@ -1,5 +1,96 @@
 $(document).ready(function() { 
 
+    $('#login').blur(function(){
+        var input = $(this);       
+        var login_length = input.val().length;
+        if(login_length >= 3 && login_length <= 15){ 
+            input.removeClass("invalid").addClass("valid");
+            input.next('.komunikat').text("Wprowadzono poprawną nazwę.");       
+        }
+        else{   
+            input.removeClass("valid").addClass("invalid");
+            input.next('.komunikat').text("Nazwa musi mieć więcej niż 3 i mniej niż 15 znaków!");		
+		}
+    });
+
+    $('#haslo1').blur(function(){
+        var input = $(this);     
+        var login = input.val()       
+        var passwordReg = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+               
+        if( passwordReg.test(login)) {    
+            input.removeClass("invalid").addClass("valid");
+            input.next('.komunikat').text("Wprowadzono poprawne hasło.");                
+        }
+        else{   
+            input.removeClass("valid").addClass("invalid");
+            input.next('.komunikat').text("Wprowadź kombiancje co najmniej 8 cyfr, małych liter, dużych liter i znaków (takich jak !@#$%^&*)");	
+        }
+   
+    });
+
+    $('#haslo2').blur(function(){
+        var input = $(this);     
+        
+        if($("#haslo1").val() == $('#haslo2').val()) {
+            input.removeClass("invalid").addClass("valid");
+            input.next('.komunikat').text("Wprowadzono poprawne hasło.");    
+        }
+        else{
+            input.removeClass("valid").addClass("invalid");
+            input.next('.komunikat').text("Podane hasła nie zgadzają się.");	
+        }
+  
+    });
+    $('#imie').blur(function(){
+        var input = $(this);  
+        var imie = input.val()  
+        var onlyLetters = new RegExp("^[a-zA-Z\s]+$");  
+        
+        if(onlyLetters.test(imie)) { 
+            input.removeClass("invalid").addClass("valid");
+            input.next('.komunikat').text("Wprowadzono poprawne dane");    
+        }
+        else{
+            input.removeClass("valid").addClass("invalid");
+            input.next('.komunikat').text("Użyj kombinacji liter");	
+        }
+  
+    });
+
+    $('#nazwisko').blur(function(){
+        var input = $(this);  
+        var imie = input.val()  
+        var onlyLetters = new RegExp("^[a-zA-Z\s]+$");  
+        
+        if(onlyLetters.test(imie)) { 
+            input.removeClass("invalid").addClass("valid");
+            input.next('.komunikat').text("Wprowadzono poprawne dane");    
+        }
+        else{
+            input.removeClass("valid").addClass("invalid");
+            input.next('.komunikat').text("Użyj kombinacji liter " );	
+        }
+  
+    });
+
+    // $('#nazwisko').blur(function(){
+    //     var input = $(this);  
+    //     var imie = input.val()  
+    //     var onlyLetters = new RegExp("^[a-zA-Z\s]+$");  
+        
+    //     if(onlyLetters.test(imie)) { 
+    //         input.removeClass("invalid").addClass("valid");
+    //         input.next('.komunikat').text("Wprowadzono poprawne dane");    
+    //     }
+    //     else{
+    //         input.removeClass("valid").addClass("invalid");
+    //         input.next('.komunikat').text("Użyj kombinacji liter");	
+    //     }
+  
+    // });
+
+
     $("#zarejestruj").click(function() {
 
         var data = $(".signupForm").serialize();
@@ -20,4 +111,6 @@ $(document).ready(function() {
             $(".alert").html(response);           
         });
     });
+    
+
 });
--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php
 
 ?>
-
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -12,90 +12,99 @@
 </head>
 <body>
     
-    <form class="signupForm" method="POST">
+    <form class="signupForm" method="POST" action="">
         <b><label>Rejestracja w serwisie Car4Rent</label></b>
         <div class="field">
             <label>Login:</label>
-            <input type="text" name="login">
+            <input type="text" name="login" id="login">
+             <span class="komunikat"></span> 
         </div>
+
         <div class="field">
             <label>Hasło:</label>
-            <input type="password" name="haslo1">
+            <input type="password" name="haslo1" id="haslo1">
+            <span class="komunikat"></span>
         </div>
+
         <div class="field">
             <label>Powtórz hasło:</label>
-            <input type="password" name="haslo2">
+            <input type="password" name="haslo2" id="haslo2">
+            <span class="komunikat"></span>
         </div>
         <b><label>Dane kontaktowe:</label></b>
         <div class="field">
             <label>Imie</label>
-            <input type="text" name="imie">
+            <input type="text" name="imie" id="imie">
+            <span class="komunikat"></span>
         </div>
         <div class="field">
             <label>Nazwisko</label>
-            <input type="text" name="nazwisko">
+            <input type="text" name="nazwisko" id="nazwisko">
+            <span class="komunikat"></span>
         </div>
         <div class="field">
             <label>Nr komórkowy</label>
-            <input type="text" name="nrKom">
+            <input type="text" name="nrKom" id="nrKom"> 
+            <span class="komunikat"></span>
         </div>
         <div class="field">
             <label>Nr telefonu</label>
-            <input type="text" name="nrTel">
+            <input type="text" name="nrTel" id="nrTel">
+            <span class="komunikat"></span>
         </div>
         <div class="field">
             <label>Fax</label>
-            <input type="text" name="fax">
+            <input type="text" name="fax" id="fax">
         </div>
         <div class="field">
             <label>Email</label>
-            <input type="text" name="email">
+            <input type="text" name="email" id="email">
         </div>
         <div class="field">
             <label>Strona internetowa</label>
-            <input type="text" name="stronaInternetowa">
+            <input type="text" name="stronaInternetowa" id="stronaInternetowa">
         </div>
         <b><label>Adres:</label></b>
         <div class="field">
             <label>Miejscowość</label>
-            <input type="text" name="miejscowosc">
+            <input type="text" name="miejscowosc" id="miejscowosc">
         </div>
         <div class="field">
             <label>Województwo:</label>
-            <input type="text" name="wojewodztwo">
+            <input type="text" name="wojewodztwo" id="wojewodztwo">
         </div>
         <div class="field">
             <label>Kod pocztowy:</label>
-            <input type="text" name="kodPocztowy">
+            <input type="text" name="kodPocztowy" id="kodPocztowy">
         </div>
         <div class="field">
             <label>Ulica:</label>
-            <input type="text" name="ulica">
+            <input type="text" name="ulica" id="ulica ">
         </div>
         <div class="field">
             <label>Nr Domu:</label>
-            <input type="text" name="nr_domu">
+            <input type="text" name="nr_domu" id="nr_domu">
         </div>
         <div class="field">
             <label>Dodatkowe informacje</label>
-            <input type="text" name="dodatkoweInformacje">
+            <input type="text" name="dodatkoweInformacje" id="dodatkowe informacje">
         </div>
         <b><label>Dane firmowe</label></b>
         <div class="field">
             <label>Zaznacz jeśli jesteś klientem firmowym</label>
-            <input type="checkbox" name="czyFirma">
+            <input type="checkbox" name="czyFirma" id="czyFirma">
         </div>
         <div class="field">
             <label>Nazwa firmy</label>
-            <input type="text" name="nazwaFirmy">
+            <input type="text" name="nazwaFirmy" id="nazwaFirmy">
         </div>
         <div class="field">
             <label>Regon</label>
-            <input type="text" name="regon">
+            <input type="text" name="regon" id="regon">
         </div>
         <div class="field">
             <label>NIP</label>
-            <input type="text" name="nip">
+            <input type="text" name="nip" id="nip">
         </div>   
         <div>
         <input id="zarejestruj" type="button" name="zarejestruj" value="Utwórz konto">
