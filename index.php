a1efaca2959236edad4be64558f31d404076f224
Marcin Chmurowski <sisimoski@MacBook-Pro-Marcin.local>
2020-03-23 19:21:53 +0100

    xd

--- index.php
+++ index.php
@@ -6,10 +6,12 @@ session_start();
 <html lang="pl">
 
 <head>
-    <title>Car4You - wypożyczalnia samochodów</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Car4You - wypożyczalnia samochodów</title>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Lato|Montserrat|Nunito|Quicksand&display=swap" rel="stylesheet">
