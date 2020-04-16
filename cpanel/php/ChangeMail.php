2c5811b7b47a443575712f87eac84b26e353be01
knammert <knammert@gmail.com>
2020-04-16 16:55:57 +0200

    ZmianaMailacz1.cdn

--- cpanel/php/ChangeMail.php
+++ cpanel/php/ChangeMail.php
@@ -1,7 +1,28 @@
-<?php 
+<?php
+session_start();
 
-
-echo 'xd';
+    if(!isset($_SESSION['id'])){
+    header("Location: ../index.php");
+    }
 
 ?>
 
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <title>Car4You - Zmiana adresu email</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Lato|Montserrat|Nunito|Quicksand&display=swap" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    </head>
+<body>
+
+ Pomylsnie zmienio adres email.
+ <button class="btn btn-primary" id="changeMail" name="changeMail" type="button">Wróć na stronę główną</button>
+
+
+</body>
