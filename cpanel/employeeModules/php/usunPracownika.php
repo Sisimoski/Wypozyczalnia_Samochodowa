a5f9d8fb2573ac3028133fc942365662f3c13533
Krystian Brzoza <krystian.brzoza@outlook.com>
2020-04-21 12:07:20 +0200

    Usuwanie Pracowników - Działa

--- cpanel/employeeModules/php/usunPracownika.php
+++ cpanel/employeeModules/php/usunPracownika.php
@@ -1,9 +1,19 @@
 <?php
     require_once $_SERVER['DOCUMENT_ROOT'] . '/php/config.php';
 
+
     $id = $_POST["id"];
     //USUWANIE KONTA
-    echo "USUNIĘTO";
+    try{
+        $sth = $db->prepare("Call usun_pracownika(:id)");
+        $sth -> bindValue(":id",$id,PDO::PARAM_INT);
+        $sth -> execute();
+        echo "Usunięto pracownika";
+    }
+    catch(Exception $e){
+        echo "Wystąpił błąd"+ $e->getMessage();
+    }
+
 
     
 
