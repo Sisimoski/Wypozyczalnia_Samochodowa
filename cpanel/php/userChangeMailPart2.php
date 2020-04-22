5b0dfbe5a27563a22486da77f9aa436c01798b39
knammert <knammert@gmail.com>
2020-04-22 12:33:47 +0200

    Opytmalizacja kodu dla bazy danych - zmiana maila

--- cpanel/php/userChangeMailPart2.php
+++ cpanel/php/userChangeMailPart2.php
@@ -1,7 +1,7 @@
 <?php   
       require_once("../php/config.php");
     
-    $sth = $db->prepare('SELECT id_klient,nowy_mail FROM zmiana_maila WHERE wartosc_hash=:hash');
+    $sth = $db->prepare('SELECT id_uzytkownik,nowy_mail FROM zmiana_maila WHERE wartosc_hash=:hash');
     $sth ->bindValue(':hash',$hash,PDO::PARAM_STR);
     $sth->execute();
     $result = $sth->fetch(PDO::FETCH_ASSOC);
@@ -12,8 +12,8 @@
     $id_klient = $result['id_klient'];
     $newMail = $result['nowy_mail'];
     
-    $sth = $db->prepare('UPDATE kontakty_klienci 
-    INNER JOIN  klienci ON klienci.id_kontakt=kontakty_klienci.id_kontakt SET email=:email WHERE  id_klient=:id_klient');
+    $sth = $db->prepare('UPDATE kontakty 
+    INNER JOIN  uzytkownik ON uzytkownik.id_kontakt=kontakty.id_kontakt SET email=:email WHERE  id_uzytkownik=:id_klient');
     $sth ->bindValue(':id_klient',$id_klient,PDO::PARAM_STR);
     $sth ->bindValue(':email',$newMail,PDO::PARAM_STR);
     $sth->execute();
--- cpanel/php/userChangePassword.php
+++ cpanel/php/userChangePassword.php
@@ -6,7 +6,7 @@
     $newPswd = MD5($_POST['newPswd']);
 
           
-        $sth = $db->prepare('SELECT id_klient FROM klienci WHERE id_klient = :id_klient AND haslo = :haslo limit 1');
+        $sth = $db->prepare('SELECT id_uzytkownik FROM uzytkownik WHERE id_uzytkownik = :id_klient AND haslo = :haslo limit 1');
         $sth ->bindValue(':id_klient',$idSession,PDO::PARAM_STR);
         $sth ->bindValue(':haslo',$oldPswd,PDO::PARAM_STR);
         $sth->execute();
@@ -15,10 +15,10 @@
             die("Stare hasło nie zgadza się") ;
         }
         else {
-            $id_klient = $result['id_klient'];
+            $id_klient = $result['id_uzytkownik'];
         }
             
-        $sth = $db->prepare('UPDATE klienci SET haslo=:haslo WHERE id_klient = :id_klient');
+        $sth = $db->prepare('UPDATE uzytkownik SET haslo=:haslo WHERE id_uzytkownik = :id_klient');
         $sth ->bindValue(':haslo',$newPswd,PDO::PARAM_STR);
         $sth ->bindValue(':id_klient',$id_klient,PDO::PARAM_STR);
         $sth->execute();
