7adb791ff55326de5c7aab1a899a98e1ad1e1ceb
Krystian Brzoza <krystian.brzoza@outlook.com>
2020-04-22 23:44:20 +0200

    Commit

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 <body>
     <!-- Alert -->
     <div class="position-fixed justify-content-center d-flex" style="width:100%; z-index: 9;">
-        <div class="alert mt-3 text-center" style="width:70%; "></div>
+        <div class="alert mt-3 text-center" style="width:70%; display: none; "></div>
     </div>
     <!-- Nagłówek Navbar -->
     <section id="header">
