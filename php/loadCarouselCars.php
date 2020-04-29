c0328a56341188e1adf98c5c097e49c8287258b0
Krystian Brzoza <krystian.brzoza@outlook.com>
2020-04-29 12:28:46 +0200

    Commit Carousele

--- php/loadCarouselCars.php
+++ php/loadCarouselCars.php
@@ -1,8 +1,8 @@
 <?php
     require_once $_SERVER['DOCUMENT_ROOT'] . '/php/config.php';
 
-    $sth =$db->prepare("SELECT vin, samochod.id_specyfikacja_samochodu, producent, model, segment, sredni_koszt_wynajmu, rok, pojemnosc_silnika, moc, typ_silnika, skrzynia_biegow, fotografia FROM samochod 
-    INNER JOIN specyfikacja_samochodu ON specyfikacja_samochodu.id_specyfikacja_samochodu = samochod.id_specyfikacja_samochodu  ORDER BY samochod.id_specyfikacja_samochodu ASC limit 6");
+    $sth =$db->prepare("SELECT vin, samochod.id_specyfikacja_samochodu, producent, model, segment,cena_brutto, rok, pojemnosc_silnika, moc, typ_silnika, skrzynia_biegow, fotografia FROM samochod 
+    INNER JOIN specyfikacja_samochodu ON specyfikacja_samochodu.id_specyfikacja_samochodu = samochod.id_specyfikacja_samochodu WHERE czy_posiadany = 1  ORDER BY samochod.id_specyfikacja_samochodu ASC limit 6");
     $sth -> execute();
 
     if($sth ->rowCount() != 0){
