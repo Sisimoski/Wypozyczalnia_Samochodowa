5f8c5f835c1ccfb4ed671b4e6c09cd366f6eff79
Krystian Brzoza <krystian.brzoza@outlook.com>
2020-05-13 22:44:15 +0200

    poprawienie linkowania

--- wynajem/samochod.php
+++ wynajem/samochod.php
@@ -82,10 +82,10 @@ session_start();
             else{
                 if(isset($_SESSION['rodzaj_konta'])){
                     if($_SESSION['rodzaj_konta'] == 1 || $_SESSION['rodzaj_konta'] == 2){
-                        echo "<button id='panelKlienta' href='./cpanel/index.php' type='submit' class='btn btn-primary'>Panel Klienta</button>";
+                        echo "<button id='panelKlienta' href='/cpanel/index.php' type='submit' class='btn btn-primary'>Panel Klienta</button>";
                     }
                     if($_SESSION['rodzaj_konta'] == 3 || $_SESSION['rodzaj_konta'] == 4){
-                        echo "<button id='panelKlienta' href='./cpanel/index.php' type='submit' class='btn btn-primary'>Panel Pracownika</button>";
+                        echo "<button id='panelKlienta' href='/cpanel/index.php' type='submit' class='btn btn-primary'>Panel Pracownika</button>";
                     }
                         
                 }
