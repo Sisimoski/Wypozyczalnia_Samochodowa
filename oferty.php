d0d6b142b2992d2ec9c98c5e378516b8426ed8c4
Marcin Chmurowski <sisimoski@MacBook-Pro-Marcin.local>
2020-05-14 19:26:53 +0200

    img card tweaks

--- oferty.php
+++ oferty.php
@@ -281,7 +281,7 @@
                 <?php for($j = 0; $j < $amountOfCarsInPage; $j++) : ?>
                     <div class="col mb-4">
                         <div class="card bg-light text-center h-100">
-                            <img src="/CarPictures/<?= $pageCars[$j]['fotografia'] ?>" class="card-img-top border-bottom rounded-lg" alt="Default Card Image">
+                            <img src="/CarPictures/<?= $pageCars[$j]['fotografia'] ?>" class="card-img-top border-bottom rounded-sm" alt="Default Card Image">
                             <div class="card-body card-body-flex">
                                 <h5 class="card-title"><?= $pageCars[$j]['producent'].' '.$pageCars[$j]['model']  ?></h5>
                                 <h6 class="card-subtitle mb-2 text-muted"><?= $pageCars[$j]['cena_brutto'] ?>zł/dzień</h6>
