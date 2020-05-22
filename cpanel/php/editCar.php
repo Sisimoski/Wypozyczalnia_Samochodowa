270a8f88e3daeeb6e82cc1004089b29deecdab34
knammert <knammert@gmail.com>
2020-05-22 13:42:53 +0200

    ZmianaStatusuEdytowanegoSamochodu

--- cpanel/php/editCar.php
+++ cpanel/php/editCar.php
@@ -30,7 +30,7 @@
         $sth = $db->prepare('UPDATE specyfikacja_samochodu SET producent=:producentEdit,model=:modelEdit,rok=:rok_produkcjiEdit,
         kolor=:kolorEdit,opis=:opisEdit,cena_netto=:cena_netto,cena_brutto=:cena_brutto,
         segment=:segment,typ_silnika=:typ_silnika,moc=:moc,pojemnosc_silnika=:pojemnosc_silnika,srednie_spalenie=:srednie_spalenie,
-        skrzynia_biegow=:skrzynia_biegow,ilosc_miejsc=:ilosc_miejsc,pojemnosc_bagaznika=:pojemnosc_bagaznika,zasieg=:zasieg      
+        skrzynia_biegow=:skrzynia_biegow,ilosc_miejsc=:ilosc_miejsc,pojemnosc_bagaznika=:pojemnosc_bagaznika,zasieg=:zasieg,czy_posiadany=:czy_posiadany     
          WHERE id_specyfikacja_samochodu=:id_specyfikacja_samochodu');
         $sth ->bindValue(':producentEdit',$producentEdit,PDO::PARAM_STR);
         $sth ->bindValue(':modelEdit',$modelEdit,PDO::PARAM_STR);
@@ -49,5 +49,6 @@
         $sth ->bindValue(':pojemnosc_bagaznika',$pojemnosc_bagaznikaEdit,PDO::PARAM_STR);
         $sth ->bindValue(':zasieg',$zasiegEdit,PDO::PARAM_STR);    
         $sth ->bindValue(':id_specyfikacja_samochodu',$id_specyfikacja_samochodu,PDO::PARAM_STR);
+        $sth ->bindValue(':czy_posiadany',3,PDO::PARAM_STR);
         $sth->execute();
 ?>
